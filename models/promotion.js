ddd6a8a79664f6784b18edbe3ae102740a8d2b98
admin <admin@admins-MacBook-Pro.local>
2021-11-14 16:00:41 -0500

    task2

--- models/promotion.js
+++ models/promotion.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
-const promtionSchema = new Schema({
+const promotionSchema = new Schema({
     name: {
         type: String,
         required: true,
