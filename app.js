65af2044e85b6a1d5ddde621f3b578f720839d6b
admin <admin@admins-MacBook-Pro.local>
2021-11-26 00:46:27 -0500

    file upload

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const promotionRouter = require('./routes/promotionRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
 
 const mongoose = require('mongoose');
 const url = config.mongoUrl;
@@ -55,6 +57,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/partners', partnerRouter);
 app.use('/promotions', promotionRouter);
+app.use('/routes/uploadRouter.js', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
