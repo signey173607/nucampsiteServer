a65ed053c8d75bf297ba51d24ead3692b6e77d00
admin <admin@admins-MacBook-Pro.local>
2021-11-27 05:20:31 -0500

    fb developer 2

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: 'YOUR FACEBOOK APP ID HERE',
-        clientSecret: 'YOUR FACEBOOK APP SECRET HERE'
-    }
-}
