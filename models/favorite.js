cca2be67cf3b39c3b46481a0ec201f52c448b3eb
admin <admin@admins-MacBook-Pro.local>
2021-11-28 02:03:01 -0500

    task 1

--- models/favorite.js
+++ models/favorite.js
@@ -12,5 +12,5 @@ const favoriteSchema = new Schema({
     }]
 });
 
-module.exports = mongoose.model(favoriteSchema);
-module.exports = Favorite;
+module.exports = mongoose.model('Favorite', favoriteSchema);
+// module.exports = Favorite;
