dc7e58edad80e1f6efcf6dda2642d44eb1e11057
admin <admin@admins-MacBook-Pro.local>
2021-11-28 03:15:53 -0500

    t12

--- models/favorite.js
+++ models/favorite.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const favoriteSchema = new Schema({
-    campsites: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Campsite'
-    }],
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    campsites: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Campsite'
+    }]
+
 });
 
 module.exports = mongoose.model('Favorite', favoriteSchema);
