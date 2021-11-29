f5f3ee1f41f489009af8bfe9eeb85ba1faf69e8a
admin <admin@admins-MacBook-Pro.local>
2021-11-28 23:12:50 -0500

    fav

--- models/favorite.js
+++ models/favorite.js
@@ -15,4 +15,5 @@ const favoriteSchema = new Schema({
     timestamps: true
 });
 
-module.exports = mongoose.model('Favorite', favoriteSchema);
+const Favorite = mongoose.model('Favorite', favoriteSchema);
+module.exports = Favorite;
